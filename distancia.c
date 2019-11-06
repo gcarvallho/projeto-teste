511d7d1d42acbd113e645c75233c4afe2713b0dc
gcarvalllho <gustavo.adamor@gmail.com>
2019-11-06 16:39:24 -0300

    Entrada das coordenadas

--- distancia.c
+++ distancia.c
@@ -12,4 +12,31 @@ int main(){
 	int x1,y1,x2,y2;
 	float distancia;
 	
+	//Entrada de dados
+	printf("Informe as coordendas para o primeiro ponto: ");
+	scanf("%d %d", &x1,&y1);
+	printf("Informe as coordendas para o segundo ponto: ");
+	scanf("%d %d", &x2,&y2);	
+} 
+#include <stdio.h>
+#include <math.h>
+
+//função que calcula a distancia
+float calculaDistancia (int x1, int y1, int x2, int y2){
+	return sqrt(pow(x2-x1,2))+pow(y2-y1,2));
+}
+
+int main(){
+	
+	//definição das variaveis
+	int x1,y1,x2,y2;
+	float distancia;
+	
+	// leitura das coordendas dos pontos
+	printf("Informe as coordendas do ponto A");
+	scanf("%d %d", &x1,&yi);
+	
+	printf("Informe as coordendas do ponto B");
+	scanf("%d %d", &x2,&y2);
+	   	
 }
