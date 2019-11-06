9f8b799c97c254e9ddcb8b3b9c63702b26524129
gcarvalllho <gustavo.adamor@gmail.com>
2019-11-06 16:44:32 -0300

    Calcula a Distancia e mostra o resultado

--- distancia.c
+++ distancia.c
@@ -17,6 +17,12 @@ int main(){
 	scanf("%d %d", &x1,&y1);
 	printf("Informe as coordendas para o segundo ponto: ");
 	scanf("%d %d", &x2,&y2);	
+	
+	//calculo da distancia entre os pontos
+	distancia = caulculoDistancia(x1,x1,x2,y2);
+	
+	//mostra o resultado
+	printf("Distancia: %f", distancia);
 } 
 #include <stdio.h>
 #include <math.h>
