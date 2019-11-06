d9919ba97ff9038ef4d37f5fa7efb711997d02c5
gcarvalllho <gustavo.adamor@gmail.com>
2019-11-06 16:55:55 -0300

    Final

--- distancia.c
+++ distancia.c
@@ -34,15 +34,5 @@ float calculaDistancia (int x1, int y1, int x2, int y2){
 
 int main(){
 	
-	//definição das variaveis
-	int x1,y1,x2,y2;
-	float distancia;
 	
-	// leitura das coordendas dos pontos
-	printf("Informe as coordendas do ponto A");
-	scanf("%d %d", &x1,&yi);
-	
-	printf("Informe as coordendas do ponto B");
-	scanf("%d %d", &x2,&y2);
-	   	
 }
